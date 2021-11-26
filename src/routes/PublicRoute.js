d471684e688247a03d6f0300f38332a213b8a863
Evgeniya Bayda <baje75128@gmail.com>
2021-11-26 10:33:59 +0200

    solution route

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -1,5 +1,7 @@
 import { Navigate } from 'react-router-dom';
 
 export default function PublicRoute({ isAuth, component: Component }) {
-  return <>{isAuth ? <Navigate to="/contacts" /> : <Component />}</>; //здесь добавила /contacts вместо /,перестали идти бесконечные запросы,но не рендерится homepage
+  return (
+    <>{isAuth ? <Navigate to="/contacts" /> && <Component /> : <Component />}</>
+  ); //здесь добавила /contacts вместо /,перестали идти бесконечные запросы,но не рендерится homepage
 }
