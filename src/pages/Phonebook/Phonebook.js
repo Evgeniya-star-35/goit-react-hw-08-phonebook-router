45beb33b18f5d90a6acfcefcf0305af64eb96f08
Evgeniya Bayda <baje75128@gmail.com>
2021-11-28 15:23:11 +0200

    Update Phonebook.js

--- src/pages/Phonebook/Phonebook.js
+++ src/pages/Phonebook/Phonebook.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts } from '../../redux/Phonebook/phonebook-selectors';
 import { fetchContacts } from '../../redux/Phonebook/phonebook-operations';
 import Form from '../../components/Form';
 import Filter from '../../components/Filter';
@@ -7,6 +8,7 @@ import ContactList from '../../components/ContactsList';
 import s from './Phonebook.module.css';
 
 export default function Phonebook() {
+  const contacts = useSelector(getContacts);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchContacts());
@@ -15,9 +17,14 @@ export default function Phonebook() {
     <>
       <h2 className={s.title}>Phonebook</h2>
       <Form />
-      <Filter />
-      <h3 className={s.contactTitle}>Contacts</h3>
-      <ContactList />
+
+      {contacts.length > 0 && (
+        <>
+          <Filter />
+          <h3 className={s.contactTitle}>Contacts</h3>
+          <ContactList />
+        </>
+      )}
     </>
   );
 }
